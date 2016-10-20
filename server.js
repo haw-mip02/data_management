cd3c5b7a56506b891b280f7c47f28f099e6f96d2
ColinS <sames.colin@gmail.com>
2016-10-20 20:15:23 +0200

    Fixes use of functions of an older express version.

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ var express = require('express'),
 
 var app = express();
 
-app.configure(function () {
-    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser());
-});
-
 app.get('/wines', wine.findAll);
 app.get('/wines/:id', wine.findById);
 app.post('/wines', wine.addWine);

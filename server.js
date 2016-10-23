2f23a7a8416e76bc919395943b81a869eb9b61af
ColinS <sames.colin@gmail.com>
2016-10-23 20:42:42 +0200

    Deletes obsolete /wines examples.

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    wine = require('./routes/wines'),
     tweet = require('./routes/tweets');
 
 
 var app = express();
 
 app.use(bodyParser.json());
-
-app.get('/wines', wine.findAll);
-app.get('/wines/:id', wine.findById);
-app.post('/wines', wine.addWine);
-app.put('/wines/:id', wine.updateWine);
-app.delete('/wines/:id', wine.deleteWine);
 
 app.get('/tweets', tweet.findAll);
 app.get('/tweets/:id', tweet.findById);
